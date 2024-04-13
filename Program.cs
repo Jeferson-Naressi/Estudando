ba5220dacf9aaec98fb7d2a7e056e9e3d5296f2b
Jeferson <jeferson143@hotmail.com>
2024-04-12 23:22:55 -0300

    Atualizando

--- Program.cs
+++ Program.cs
@@ -3,23 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
 
-
 class Program
 {
     static void Main(string[] args)
     {
-        // Lista para armazenar os itens
         List<string> itens = new List<string>();
 
-        // TODO: Solicite os itens ao usuário
         for (int contato = 0; contato < 3 ; contato++)
         {
             Console.Write("Digite seu nome: ");
             string nome = string.Concat(Console.ReadLine());
             itens.Add(nome);
         }
-
-        // Exibe a lista de itens
         Console.WriteLine("Lista de itens:");
         foreach (string item in itens)
         {
