27cde584dbbcc7aa10c3c6d9447de514d475ac95
Jeferson <jeferson143@hotmail.com>
2024-04-12 23:16:50 -0300

    Atualizando

--- Program.cs
+++ Program.cs
@@ -8,20 +8,20 @@ public class Pessoa ()
 {
 }
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         // Lista para armazenar os itens
-//         List<string> itens = new List<string>();
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lista para armazenar os itens
+        List<string> itens = new List<string>();
 
-//         // TODO: Solicite os itens ao usuário
-//         for (int contato = 0; contato < 3 ; contato++)
-//         {
-//             Console.Write("Digite seu nome: ");
-//             string nome = string.Concat(Console.ReadLine());
-//             itens.Add(nome);
-//         }
+        // TODO: Solicite os itens ao usuário
+        for (int contato = 0; contato < 3 ; contato++)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = string.Concat(Console.ReadLine());
+            itens.Add(nome);
+        }
 
 //         // Exibe a lista de itens
 //         Console.WriteLine("Lista de itens:");
