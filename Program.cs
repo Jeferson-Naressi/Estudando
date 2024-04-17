6687aecc919fcd30e48968ac1a13b959f75a7220
Jeferson <jeferson143@hotmail.com>
2024-04-16 23:08:44 -0300

    Atualizando

--- Program.cs
+++ Program.cs
@@ -2,23 +2,67 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
+using Newtonsoft.Json;
 
 class Program
 {
+    static bool VerificarAtributo(string atributo, int valorMinimo, int valorMaximo, int valorAtributo)
+    {
+        if (valorAtributo >= valorMinimo && valorAtributo <= valorMaximo){
+            return "O valor do atributo está dentro do intervalo especificado";
+        }else "O valor do atributo está fora do intervalo especificado";
+    }
+    
     static void Main(string[] args)
     {
-        List<string> itens = new List<string>();
+        string atributo = Console.ReadLine();
 
-        for (int contato = 0; contato < 3 ; contato++)
-        {
-            Console.Write("Digite seu nome: ");
-            string nome = string.Concat(Console.ReadLine());
-            itens.Add(nome);
-        }
-        Console.WriteLine("Lista de itens:");
-        foreach (string item in itens)
-        {
-            Console.WriteLine($"- {item}"); 
-        }
+        int valorMinimo = int.Parse(Console.ReadLine());
+
+        int valorMaximo = int.Parse(Console.ReadLine());
+
+        int valorAtributo = int.Parse(Console.ReadLine());
+
+        VerificarAtributo(atributo, valorMinimo, valorMaximo, valorAtributo);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         List<string> itens = new List<string>();
+
+//         for (int contato = 0; contato < 3 ; contato++)
+//         {
+//             Console.Write("Digite seu nome: ");
+//             string nome = string.Concat(Console.ReadLine());
+//             itens.Add(nome);
+//         }
+//         Console.WriteLine("Lista de itens:");
+//         foreach (string item in itens)
+//         {
+//             Console.WriteLine($"- {item}"); 
+//         }
+//     }
+// }
+
+
+// Considero-me cusioso e esforçado, tenho facilidade em aprender as coisas, adaptabilidade, dedicação ao trabalho e bom relacionamento. Em busca de adquirir uma nova formação profissional na área de tecnologia, a qual venho pesquisando e me identificando cada vez mais. Dessa forma, venho me aprofundado nas áreas de tecnologias, e nesse caminhar tenho feito cursos e projetos referente a linguagens de programação e de marcação, segurança de rede, assim como, participando de programas de capacitação para me tornar um profissional de Tecnologia de Informação.
--- obj/Debug/net8.0/Estudando.AssemblyInfo.cs
+++ obj/Debug/net8.0/Estudando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estudando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83e778fb47ba6164f67d08d25280b5a0a1c196b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5220dacf9aaec98fb7d2a7e056e9e3d5296f2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estudando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estudando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
